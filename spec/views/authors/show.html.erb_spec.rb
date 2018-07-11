573580f80bbd8cab6628b7652a5e475974c0a414
Villy Siu <villysiu@gmail.com>
2018-07-11 21:31:55 +0000

    automatically backed up by learn

--- spec/views/authors/show.html.erb_spec.rb
+++ spec/views/authors/show.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "authors/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+#RSpec.describe "authors/show.html.erb", type: :view do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+RSpec.describe "show_authors" do
+  let(:author){ Author.create(name: 'Bobby', genre: 'scifi', bio: 'not available yet') }
+
+  it "renders author information" do
+    view.lookup_context.prefixes = %w[authors]
+    assign(:author, author)
+    render :template => "authors/show.html.erb"
+    expect(rendered).to include('Bobby')
+  end
 end
